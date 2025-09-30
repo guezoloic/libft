afc50a371853cd7de0136c8f103478faed81c3c2
Loïc GUEZO <loicguezo@gmail.com>
2025-09-30 19:01:09 +0200

    fix: update function name

--- libft.h
+++ libft.h
@@ -4,47 +4,47 @@
 # include <stdlib.h>
 
 /*
-    The ft_isalpha() function tests for
-    any character for which isupper(3) or
-    islower(3) is true.
+	The ft_isalpha() function tests for
+	any character for which isupper(3) or
+	islower(3) is true.
  */
 int	ft_isalpha(int c);
 /*
-    The ft_isdigit() function tests 
-    for a decimal digit character.  
-    Regardless of locale, this 
-    includes the following characters 
-    only: '0' '1' '2' '3' '4' '5' '6'
-    '7' '8' '9'
+	The ft_isdigit() function tests 
+	for a decimal digit character.  
+	Regardless of locale, this 
+	includes the following characters 
+	only: '0' '1' '2' '3' '4' '5' '6'
+	'7' '8' '9'
  */
 int	ft_isdigit(int c);
 
 /*
-    The ft_isalnum() function tests 
-    for any character for which
-    isalpha(3) or isdigit(3) is true.
+	The ft_isalnum() function tests 
+	for any character for which
+	isalpha(3) or isdigit(3) is true.
  */
 int ft_isalnum(int c);
 
 /*  
-    The ft_isascii() function tests 
-    for an ASCII character, which is
-    any character between 0 and octal
-    0177 inclusive. 
+	The ft_isascii() function tests 
+	for an ASCII character, which is
+	any character between 0 and octal
+	0177 inclusive. 
  */
 int	ft_isascii(int c);
 
 /*
-    The ft_isprint() function tests for 
-    any printing character, including
-    space (‘ ’).
+	The ft_isprint() function tests for 
+	any printing character, including
+	space (‘ ’).
 */
 int	ft_isprint(int c);
 
 /*
-    The strlen() function computes the 
-    length of the string s.
+	The ft_strlen() function computes 
+	the length of the string s.
 */
-size_t	strlen(const char *s);
+size_t	ft_strlen(const char *s);
 
 #endif
