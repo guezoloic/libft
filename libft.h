b4d1a32d65645e8e8a96c77bf5579ddef56a747e
Loïc GUEZO <loicguezo@gmail.com>
2025-10-05 22:00:14 +0200

    feat: add ft_toupper function

--- libft.h
+++ libft.h
@@ -88,4 +88,13 @@ void	*ft_memmove(void *dst, const void *src, size_t n);
 */
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
+// size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+/*
+	The toupper() function converts a 
+	lower-case letter to the 
+	corresponding upper-case letter.
+*/
+int	ft_toupper(int c);
+
 #endif
