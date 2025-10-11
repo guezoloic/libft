327d115d418215c2a694b28ec1e6d8e09ba75a8a
Loïc GUEZO <loicguezo@gmail.com>
2025-10-11 21:59:34 +0200

    fix: follow norminette norms

--- ft_memchr.c
+++ ft_memchr.c
@@ -2,17 +2,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	const unsigned char *ptr;
-	
+	size_t				i;
+	const unsigned char	*ptr;
+
 	i = 0;
 	ptr = (const unsigned char *)s;
 	while (i < n)
 	{
 		if ((unsigned char)c == ptr[i])
-			return ((void*)(ptr+i));
+			return ((void *)(ptr + i));
 		i++;
 	}
-
-	return NULL;
+	return (NULL);
 }
