72bb38876ba71a3f5e5bd79036e6f94cbbca0cdd
Loïc GUEZO <loicguezo@gmail.com>
2025-10-22 19:00:29 +0200

    feat(ft_memcmp): add ft_memcmp function

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,10 +1,16 @@
 #include "libft.h"
 
-// int	ft_memcmp(const void *s1, const void *s2, size_t n)
-// {
-// 	const unsigned char *s1_ptr;
-// 	const unsigned char *s2_ptr;
-// 	size_t				i;
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*c1;
+	char	*c2;
+	int		i;
 
-// 	while ()
-// }
+	c1 = (char *)s1;
+	c2 = (char *)s2;
+	i = 0;
+	while (i < n && c1[i] == c2[i])
+		i++;
+	if (i == n) return (0);
+	return (c1[i] - c2[i]);
+}
